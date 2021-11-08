cd964486722f696bae9f36eea34d810689fd90d5
Alejahndro <alejahndro02@gmail.com>
2021-11-08 13:21:43 -0600

    Se declaro la variable mainMenu y en el metodo ngOniniti se declaran los valores de los arreglos correspondientes a edfaultOptions,accessLink y customOptions

--- src/app/shared/components/side-bar/side-bar.component.ts
+++ src/app/shared/components/side-bar/side-bar.component.ts
@@ -6,10 +6,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./side-bar.component.css']
 })
 export class SideBarComponent implements OnInit {
+mainMenu:{
+  defaultOptions:Array<any>,
+  accessLink:Array<any>,
+} = { defaultOptions:[], accessLink:[] };
+customOptions:Array<any> = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.mainMenu.defaultOptions = [
+    {
+      name:'Home',
+      icon:'uil uil-estate',
+      routes:['/']
+    },
+    {
+      name:'Buscar',
+      icon:'uil uil-search',
+      routes:['/', 'history']
+    },
+    {
+      name:'Tu Biblioteca',
+      icon:'uil uil-chart',
+      routes:['/', 'favorites']
+    }
+    ];
+    this.mainMenu.accessLink = [
+      {
+        name: 'Crear Lista',
+        icon: 'uil uil-square'
+      },
+      {
+        name: 'Canciones que te gustan',
+        icon: 'uil uil-heart-medical'
+      }
+    ];
+    this.customOptions = [
+      {
+        name:'Mi Lista °1',
+        rouer:['/']
+      },
+      {
+        name:'Mi Lista °2',
+        rouer:['/']
+      },
+      {
+        name:'Mi Lista °3',
+        rouer:['/']
+      },
+      {
+        name:'Mi Lista °4',
+        rouer:['/']
+      },
+      {
+        name:'Mi Lista °5',
+        rouer:['/']
+      },
+    ]
   }
 
 }
