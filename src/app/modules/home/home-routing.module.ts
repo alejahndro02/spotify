8909fb0e803fb890f40c9d5fb3620d1676ae6ed3
Alejahndro <alejahndro02@gmail.com>
2021-11-08 22:26:02 -0600

    SE agregaron las rutas (tracks, history, favorites) que navegaran dentro del componente de home

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,9 +1,22 @@
-import { HomePageComponent } from './pages/home-page/home-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path:'', component:HomePageComponent}
+  {
+    path:'tracks',
+    loadChildren:() => import('@modules/tracks/tracks.module')
+    .then(m => m.TracksModule)
+  },
+  {
+    path:'history',
+    loadChildren:() => import('@modules/history/history.module')
+    .then(m => m.HistoryModule)
+  },
+  {
+    path:'favorites',
+    loadChildren:() => import('@modules/favorites/favorites.module')
+    .then(m => m.FavoritesModule)
+  }
 ];
 
 @NgModule({
