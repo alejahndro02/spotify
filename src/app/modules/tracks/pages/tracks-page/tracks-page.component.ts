e3726b6d3f3c6fa6d67275addab2d2a5bab390dd
Alejahndro <alejahndro02@gmail.com>
2021-11-09 23:37:32 -0600

    SE importa el modelo de TackModel y se importa la base de datos y se le asigna a una varible(dataRaw), a la variable mockTracksList se le asigna un valor de tipo array que contendra los valores de trackModel tambien se declara una constante 'data' dentro del metodo ngOnInit y se le asigna el valor que viene de de la variable dataRaw

--- src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
+++ src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
@@ -1,17 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import { TrackModel } from '@core/models/tracks.model';
+//Se asigna todo el valor de la data a la variable data 
+import * as dataRaw from '@data/tracks.json'
 @Component({
   selector: 'app-tracks-page',
   templateUrl: './tracks-page.component.html',
   styleUrls: ['./tracks-page.component.css']
 })
 export class TracksPageComponent implements OnInit {
-mockTracksList=[
-  {name:'bebe(oficial)'}
-]
+  mockTracksList:Array<TrackModel> =[]
   constructor() { }
 
   ngOnInit(): void {
+    const {data} : any = (dataRaw as any).default
+    this.mockTracksList = data;
   }
 
 }
