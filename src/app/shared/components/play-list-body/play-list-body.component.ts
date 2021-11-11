ad082eab90cd5d1efdf37b56ce9d83949cd34361
Alejahndro <alejahndro02@gmail.com>
2021-11-10 19:50:21 -0600

    Se declara la variable OptionSort donse se le pasa el parametro property de tipo string o nulo y el parametro order de tipo string asu vez se inicializa con la propiedad como nula y el ord asc, por ultimo se crea el metodo changeSort donde se le pasa como parametro la propiedad dependiedo del resultado renderizara el resultado de forma ascendete o descendente

--- src/app/shared/components/play-list-body/play-list-body.component.ts
+++ src/app/shared/components/play-list-body/play-list-body.component.ts
@@ -9,12 +9,18 @@ import * as dataRaw from '@data/tracks.json'
 export class PlayListBodyComponent implements OnInit {
   // tracks:Array<TrackModel> = [] ;//Esto es lo equivalente a escribir el comando de abajo 
   tracks:TrackModel [] = []
-
+  optionSort:{property:string | null, order:string} = {property:null, order:'asc'}
   constructor() { }
 
   ngOnInit(): void {
     const {data} : any = (dataRaw as any).default
     this.tracks = data;
   }
-
+ changeSort(property:string):void{
+   const {order} = this.optionSort;
+   this.optionSort = {
+    property,
+    order: order === 'asc' ? 'desc' : 'asc'
+   }
+ }
 }
