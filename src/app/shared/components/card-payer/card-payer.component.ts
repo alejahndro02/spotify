c87826dc017bac11d2dfe13cddb298ea1634fb81
Alejahndro <alejahndro02@gmail.com>
2021-11-11 08:10:04 -0600

    Se impora el servicio MultimediaService y se inyecta como un servicio privado dentro del contructor, se crea un metodo (sendPlayer()) que se le pasa como argumento una variable track de tipo TrackModel que sera emitido por medio del metodo callbackMediaPLayer

--- src/app/shared/components/card-payer/card-payer.component.ts
+++ src/app/shared/components/card-payer/card-payer.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { TrackModel } from '@core/models/tracks.model';
+import { MultimediaService } from '@shared/services/multimedia.service';
 
 @Component({
   selector: 'app-card-payer',
@@ -9,9 +10,11 @@ import { TrackModel } from '@core/models/tracks.model';
 export class CardPayerComponent implements OnInit {
 @Input() mode:'small' | 'big' = 'small';
 @Input() track:TrackModel ={_id:0,name:'',album:'',cover:'', url:''};
-  constructor() { }
+  constructor(private multimediaService: MultimediaService) { }
 
   ngOnInit(): void {
   }
-
+  sendPlayer(track:TrackModel):void{
+    this.multimediaService.callbackMediaPlayer.emit(track);
+  }
 }
