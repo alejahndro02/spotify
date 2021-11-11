17b58f4ac080a562169bee10071160b261543b9e
Alejahndro <alejahndro02@gmail.com>
2021-11-11 09:07:51 -0600

    Se importo el modulo Subscription desde rxjs, se crea una variable donde se almacenaran una lista de observables refentes al componente media, a la variable observerMedia$ de ceclara de tipo Subscription y el contednido de la variable se le pasa como valor al array de la variable lisObserversMedia$, por ultimo se implementa el ciclo de vida OnDestroy que ejecutara una unsubscription de la lista de observables llamada listObserversMEdia$

--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { TrackModel } from '@core/models/tracks.model';
 import { MultimediaService } from '@shared/services/multimedia.service';
+import { Subscription  } from 'rxjs';
 
 @Component({
   selector: 'app-media-player',
   templateUrl: './media-player.component.html',
   styleUrls: ['./media-player.component.css']
 })
-export class MediaPlayerComponent implements OnInit {
+export class MediaPlayerComponent implements OnInit, OnDestroy {
 mockCover: TrackModel ={
   cover:'https://i.scdn.co/image/ab67616d0000b27345ca41b0d2352242c7c9d4bc',
   album:'Gioli & Assia',
@@ -15,13 +16,18 @@ mockCover: TrackModel ={
   _id:1,
   url:'http://localhost/track.mp3'
 }
+listObserversMedia$:Array<any> = []
   constructor(private multimediaService: MultimediaService) { }
 
   ngOnInit(): void {
-    const observerMedia$ = this.multimediaService.callbackMediaPlayer
+    const observerMedia$: Subscription = this.multimediaService.callbackMediaPlayer
     .subscribe((response: TrackModel)=>{
       console.log('Recibiendo cancion', response.name);
     })
+    this.listObserversMedia$ = [observerMedia$]
   }
-
+  ngOnDestroy(): void {
+      this.listObserversMedia$.forEach(u => u.unsubscribe())
+  }
+  
 }
