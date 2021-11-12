c08b474e73c18dbbabf0dd8fe473fe31cc1dc12a
Alejahndro <alejahndro02@gmail.com>
2021-11-11 21:09:53 -0600

    se delcaran las variables de tracksTrending y tracksRandom como un tipo de dato TrackModel que se inicaliza como un array vacio y otra variable listObserverTrack de tipo array con el parameto de subscription, se inyecta el servicio de trackService, dentro del metodo ngOnInit se le asigna a la variable observerTrackTrending$ la data y se subscribe, este mismo proceso se le asigna a la variable ObserverTrackRandom , se declara el metodo ngOnDestroy para poder unsubscibe al observable

--- src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
+++ src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
@@ -1,21 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { TrackService } from './../../services/track.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { TrackModel } from '@core/models/tracks.model';
+import { Subscription } from 'rxjs';
 //Se asigna todo el valor de la data a la variable data 
-import * as dataRaw from '@data/tracks.json'
+
 @Component({
   selector: 'app-tracks-page',
   templateUrl: './tracks-page.component.html',
   styleUrls: ['./tracks-page.component.css']
 })
-export class TracksPageComponent implements OnInit {
-  // mockTracksList:Array<TrackModel> =[]
-  mockTracksList:TrackModel[] =[]
+export class TracksPageComponent implements OnInit, OnDestroy {
+  tracksTrending:TrackModel[] = [];
+  tracksRandom:TrackModel[] = [];
+  listObserverTrack$:Array<Subscription> = [];
 
-  constructor() { }
+  constructor(private trackService:TrackService) { }
 
   ngOnInit(): void {
-    const {data} : any = (dataRaw as any).default
-    this.mockTracksList = data;
+    console.log();
+    const ObserverTrackTrending$ = this.trackService.dataTracksTrendind$
+    .subscribe(response=>{
+      this.tracksTrending = response;
+      this.tracksRandom = response;
+      console.log('canciones trending', response);
+    })
+    const ObserverTrackRandom$ = this.trackService.dataTracksRandom$
+    .subscribe(response => {
+      this.tracksRandom = [...this.tracksRandom, ...response]
+      console.log('canciones Random', response);
+    })
+    this.listObserverTrack$=[ObserverTrackTrending$, ObserverTrackRandom$ ];
   }
-
+ngOnDestroy():void{
+  this.listObserverTrack$.forEach(u => u.unsubscribe());
+}
 }
