651190c0617a238c30532d4ff699ac2aa70d76e8
Alejahndro <alejahndro02@gmail.com>
2021-11-12 09:18:49 -0600

    Se reemplaza los datos provenentes de la data proveniente del archivo Json, se importa los valores del archivo environmet, se declara una variabel de solo lectura con la url de la api y por ultimo se crea un metodo get que retorna los valores porvenentes de la url

--- src/app/modules/tracks/services/track.service.ts
+++ src/app/modules/tracks/services/track.service.ts
@@ -1,34 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { TrackModel } from '@core/models/tracks.model';
 import { Observable, of } from 'rxjs';
-import * as dataRaw from '@data/tracks.json'
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TrackService {
+  //hace referencia a la api delcarada en el archivo envronment
+  private readonly URL = environment.api;
 
- dataTracksTrendind$:Observable<TrackModel[]> =  of([]);
- 
- //Otra opcion de crear observables
- dataTracksRandom$:Observable<any> =  of([]);
-
- constructor() {
-    const {data} : any = (dataRaw as any).default;
-
-    this.dataTracksTrendind$ = of(data);
-    
-    this.dataTracksRandom$ = new Observable(observer =>{
-      const trackExample: TrackModel = {
-        _id:9,
-        name:'leve',
-        album:'cartel de santa',
-        url: 'http://',
-        cover:'https://studiosol-a.akamaihd.net/uploadfile/letras/fotos/f/2/8/8/f28893d1d7f27c22764f28b32375960e.jpg'
-      }
-    
-      setTimeout(() => {
-        observer.next([trackExample]);
-      }, 4000);
-    })
-}}
+ constructor(private httpCliente:HttpClient) {}
+  getAllTracks$():Observable<any>{
+    return this.httpCliente.get(`${URL}/tracks`);
+  }
+}
