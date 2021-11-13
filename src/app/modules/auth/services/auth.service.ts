a559a29c2d0627f6854f79852bb4f2c616a767b8
Alejahndro <alejahndro02@gmail.com>
2021-11-13 12:24:46 -0600

    Se importraron los modulos Observable, HttpClient y el archivo de enviroment, se decalro la variable te tipo lectua Url asignandole el valor proveniente de environmet.api, se inyecto HttpClient como  dependencia privada para ser usada dento del metodo sendcredentials que retorna un metodo post

--- src/app/modules/auth/services/auth.service.ts
+++ src/app/modules/auth/services/auth.service.ts
@@ -1,13 +1,22 @@
+import { Observable } from 'rxjs';
+import { environment } from './../../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private readonly URL = environment.api;
+  constructor(private http:HttpClient) { 
+    
+  }
 
-  constructor() { }
-
-  sendCredentials(email:string, password:string): void {
-    console.log('ok, ok' ,email, password);
+  sendCredentials(email:string, password:string): Observable<any>{
+    const body={
+      email,
+      password
+    }
+    return this.http.post(`${this.URL}/auth/login`, body);
   }
 }
