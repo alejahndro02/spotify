91eb9a74019e106ecc0eb123af91e9969fdc8bf5
Alejahndro <alejahndro02@gmail.com>
2021-11-13 21:36:53 -0600

    Se iporta el servicio de cookie-service y se implemete por medio de un pipe y su operador tap dentro de lo que retorna el metodo sendCredentials

--- src/app/modules/auth/services/auth.service.ts
+++ src/app/modules/auth/services/auth.service.ts
@@ -12,8 +12,7 @@ export class AuthService {
   private readonly URL = environment.api;
   constructor(
     private http:HttpClient, 
-    private cookie: CookieService) { 
-  }
+    private cookie: CookieService) { }
 
   sendCredentials(email:string, password:string): Observable<any>{
     const body={
