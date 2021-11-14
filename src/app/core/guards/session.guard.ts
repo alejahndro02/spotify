d6e36f76d3d36588c0c2a768b50b01fad653294c
Alejahndro <alejahndro02@gmail.com>
2021-11-13 23:10:13 -0600

    Se implemento un constructor dentro de la clase CanActivate donde se inyectan los servicios de cookies y router, se crea una funcion de nombre checkCookieSession donde se evalua si hay una cookie de nombre tokenComponent en caso de no estar reenvia la anvegacion hacia /auth/login

--- src/app/core/guards/session.guard.ts
+++ src/app/core/guards/session.guard.ts
@@ -1,15 +1,36 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SessionGuard implements CanActivate {
+  //Se Implemeta un constructor para poder guardar el token dentro del cookieService
+  constructor(
+    private cookie:CookieService,
+    private router: Router){}
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    return this.checkCookieSession();//Se ejecuta la fucncion de checkCookieSession
   }
-  
+
+  //Se crea una funcion paa revisar las cookies que hay en el navegador 
+  checkCookieSession():boolean{
+    try {
+      // return this.cookie.check('token');//retorna un booleano que de pednera si hay un tokenSession con el nombre de "token"
+      const token:boolean = this.cookie.check('tokenComponent');
+
+      if(!token){
+        this.router.navigate(['/','auth']);
+        return false
+      }
+      return token;
+    } catch (error) {
+      console.log('Upsss Algo anda mal',this.cookie);
+      return !true;
+    }
+  }
 }
