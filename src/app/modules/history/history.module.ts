e0a2038b2825b73f1673552decb456135d653fd7
Alejahndro <alejahndro02@gmail.com>
2021-11-14 13:41:59 -0600

    Se importaron los modulos  sharemodule y formsModule

--- src/app/modules/history/history.module.ts
+++ src/app/modules/history/history.module.ts
@@ -4,16 +4,20 @@ import { CommonModule } from '@angular/common';
 import { HistoryRoutingModule } from './history-routing.module';
 import { HistoryPageComponent } from './pages/history-page/history-page.component';
 import { SearchComponent } from './components/search/search.component';
+import { SharedModule } from '@shared/shared.module';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     HistoryPageComponent,
-    SearchComponent
+    SearchComponent,
   ],
   imports: [
     CommonModule,
-    HistoryRoutingModule
+    HistoryRoutingModule,
+    SharedModule,
+    FormsModule
   ]
 })
 export class HistoryModule { }
