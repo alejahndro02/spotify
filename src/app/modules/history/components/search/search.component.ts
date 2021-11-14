28b712c3aa14548e10138f0fbcd56c5bf6b281a8
Alejahndro <alejahndro02@gmail.com>
2021-11-14 13:51:30 -0600

    Se declaro la variable src como un string y se inicializa en vacio, se delcara la funcion seachracks como argumento se le pasa una variable palabra de tipo string  y esta se condiciona si es mayor a 3 letras envia la peticion a la api

--- src/app/modules/history/components/search/search.component.ts
+++ src/app/modules/history/components/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-search',
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-
+src: string = '';
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  searchTracks(palabra:string):void{
+    if(palabra.length >=3){
+      console.log('oalabra que buscas', palabra);
+    }
+    
+  }
 }
