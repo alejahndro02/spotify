8f4ea6351e0b2405097748d2978d6776c32fe3a8
Alejahndro <alejahndro02@gmail.com>
2021-11-19 21:39:15 -0600

    Se declara el selector @outputy se le asigna la variable callBackData que es de tipo eventEmitter, dentro de la funcion searchTracks al callBackData se le asigna una funcion emit que tiene como parametro la palabra a buscar

--- src/app/modules/history/components/search/search.component.ts
+++ src/app/modules/history/components/search/search.component.ts
@@ -6,15 +6,17 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-src: string = '';
+  @Output() callBackData: EventEmitter<any> = new EventEmitter();
+  src: string = '';
+
   constructor() { }
 
   ngOnInit(): void {
   }
   searchTracks(palabra:string):void{
     if(palabra.length >=3){
+      this.callBackData.emit(palabra);
       console.log('oalabra que buscas', palabra);
     }
-    
   }
 }
