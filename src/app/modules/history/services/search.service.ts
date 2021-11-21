5e7db05c811d7764400e1960629071b0f9846711
Alejahndro <alejahndro02@gmail.com>
2021-11-21 12:19:53 -0600

    Se importo el operador map de la libreria de rxjs/operators  y se declara un pipe uando el operador map para retornarn la data proveniente de la api

--- src/app/modules/history/services/search.service.ts
+++ src/app/modules/history/services/search.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../../environments/environment';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,9 @@ private readonly URL = environment.api
 constructor(private http: HttpClient) { }
 
   searchTracks$(palabra:string):Observable<any>{
-    return this.http.get(`${this.URL}/tracks?src=${palabra}`);
+    return this.http.get(`${this.URL}/tracks?src=${palabra}`)
+    .pipe(
+      map((dataRaw:any)=> dataRaw.data)
+    );
   }
 }
