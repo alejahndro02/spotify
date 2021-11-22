3c28a546a4c392940164aaea446273750218d9a3
Alejahndro <alejahndro02@gmail.com>
2021-11-22 10:57:42 -0600

    Se cambio la inyeccicon del servicio de privado a publico y se elimino el objeto mockCover de tipo trackModel

--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { TrackModel } from '@core/models/tracks.model';
 import { MultimediaService } from '@shared/services/multimedia.service';
-import { Subscription  } from 'rxjs';
 
 @Component({
   selector: 'app-media-player',
@@ -9,25 +7,10 @@ import { Subscription  } from 'rxjs';
   styleUrls: ['./media-player.component.css']
 })
 export class MediaPlayerComponent implements OnInit, OnDestroy {
-mockCover: TrackModel ={
-  cover:'https://i.scdn.co/image/ab67616d0000b27345ca41b0d2352242c7c9d4bc',
-  album:'Gioli & Assia',
-  name:'Babe(oficial)',
-  _id:1,
-  url:'http://localhost/track.mp3'
-}
 listObserversMedia$:Array<any> = []
-  constructor(private multimediaService: MultimediaService) { }
-
-  ngOnInit(): void {
-    const observerMedia$: Subscription = this.multimediaService.callbackMediaPlayer
-    .subscribe((response: TrackModel)=>{
-      console.log('Recibiendo cancion', response.name);
-    })
-    this.listObserversMedia$ = [observerMedia$]
-  }
+  constructor(public multimediaService: MultimediaService) { }
+  ngOnInit(): void {}
   ngOnDestroy(): void {
       this.listObserversMedia$.forEach(u => u.unsubscribe())
   }
-  
 }
