158d5bf27b260e987b0f0cc74815d837715d483c
Alejahndro <alejahndro02@gmail.com>
2021-11-22 11:04:46 -0600

    Se declararon las variables publicas trackInfo$, timeElapsed$, timeRemaining$ de tipo behaviorSubject de la libre rxjs y una variable Audio de tipo HTMLAudioElement, tambien se secrean fuciones privadas listenAllEventAudio,calculateTime, setTimeElpased, setTimeRemaining y una fucnion tipo publica setAudio

--- src/app/shared/services/multimedia.service.ts
+++ src/app/shared/services/multimedia.service.ts
@@ -1,9 +1,58 @@
 import { Injectable, EventEmitter } from '@angular/core';
+import { TrackModel } from '@core/models/tracks.model';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MultimediaService {
   callbackMediaPlayer: EventEmitter<any> = new EventEmitter<any>()
-  constructor() { }
+  public trackInfo$: BehaviorSubject<any> = new BehaviorSubject(undefined);
+  public audio: HTMLAudioElement = new Audio();
+  public timeElapsed$: BehaviorSubject<string> = new BehaviorSubject('00 : 00');
+  public timeRemaining$: BehaviorSubject<string> = new BehaviorSubject('-00 : 00');
+
+  constructor() {
+this.trackInfo$.subscribe(responseOK =>{
+  // this.audio = new Audio();
+  if(responseOK){
+    this.setAudio(responseOK);
+  }
+  this.listenAllEventAudio();
+})
+}
+  //fuciones Privadas
+  private listenAllEventAudio():void{
+    this.audio.addEventListener('timeupdate',this.calculateTime, false)
+  }
+  private calculateTime= ()=>{
+    console.log('disparando evento');
+    const { duration, currentTime }= this.audio;
+    console.table( [ duration, currentTime ] );
+    this.setTimeElapsed(currentTime);
+    this.setTimeRemaining(currentTime, duration);
+  }
+  private setTimeElapsed(currentTime:number):void{
+    let seconds = Math.floor(currentTime % 60);
+    let minutes = Math.floor((currentTime / 60) % 60 );
+    const displaySeconds = (seconds < 10) ? `0 ${seconds}` : seconds;
+    const displayMinutes = (minutes < 10) ? `0 ${minutes}` : minutes;
+    const displayFormat= `${displayMinutes} : ${displaySeconds}`
+    this.timeElapsed$.next(displayFormat);
+  }  
+  private setTimeRemaining(currentTime:number, duration:number):void{
+    let timeLeft = duration - currentTime;
+    let seconds = Math.floor(timeLeft % 60);
+    let minutes = Math.floor((timeLeft / 60) % 60 );
+    const displaySeconds = (seconds < 10) ? `0 ${seconds}` : seconds;
+    const displayMinutes = (minutes < 10) ? `0 ${minutes}` : minutes;
+    const displayFormat= `-${displayMinutes} : ${displaySeconds}`
+    this.timeRemaining$.next(displayFormat);
+  }
+  //funcioiin Publica
+  public setAudio(track:TrackModel):void{
+    console.log(':):):):', track);
+    this.audio.src = track.url;
+    this.audio.play();
+  }
 }
