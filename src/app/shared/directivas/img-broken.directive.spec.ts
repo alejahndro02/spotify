329ee241a55f80dd9d7d8a104936f87675c09b41
Alejahndro <alejahndro02@gmail.com>
2021-11-22 21:44:15 -0600

    Se declara una constante como un tipo de dato ElementREf  y se pasa como argumento dentro de ImgBrokenDirective

--- src/app/shared/directivas/img-broken.directive.spec.ts
+++ src/app/shared/directivas/img-broken.directive.spec.ts
@@ -1,8 +1,12 @@
+import { ElementRef } from '@angular/core';
 import { ImgBrokenDirective } from './img-broken.directive';
 
 describe('ImgBrokenDirective', () => {
   it('should create an instance', () => {
-    const directive = new ImgBrokenDirective();
+    //Se creo la constante asignandole un new ElementRef()
+    const elHost = new ElementRef('');
+    //Requiere como valor un argumento de tipo elementRef
+    const directive = new ImgBrokenDirective(elHost);
     expect(directive).toBeTruthy();
   });
 });
