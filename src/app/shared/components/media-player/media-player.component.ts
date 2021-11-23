1fd5260b51179b6041a111bbd5ca047b3b1c2dce
Alejahndro <alejahndro02@gmail.com>
2021-11-22 20:21:32 -0600

    Por medio del @viwchild se le asigna el id ProgressBar como un tipo de elemento de referencia(ElementRef) inicializando como vacio y se declara la funcion handlePosition como argumento es el evento de tipo MouseEvent

--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { MultimediaService } from '@shared/services/multimedia.service';
 
 @Component({
@@ -7,8 +7,9 @@ import { MultimediaService } from '@shared/services/multimedia.service';
   styleUrls: ['./media-player.component.css']
 })
 export class MediaPlayerComponent implements OnInit, OnDestroy {
-listObserversMedia$:Array<any> = []
-state:string= 'paused'
+  @ViewChild('progressBar') progressBar : ElementRef = new ElementRef('');
+  listObserversMedia$:Array<any> = []
+  state:string= 'paused'
   constructor(public multimediaService: MultimediaService) { }
   ngOnInit(): void {
     const mediaObserver = this.multimediaService.playerStatus$.subscribe(status => this.state = status)
@@ -17,5 +18,13 @@ state:string= 'paused'
   }
   ngOnDestroy(): void {
       this.listObserversMedia$.forEach(u => u.unsubscribe())
+  }
+  handlePosition(event:MouseEvent):void{
+    const elNative: HTMLElement = this.progressBar.nativeElement;
+    const {clientX} = event;
+    const {x, width} = elNative.getBoundingClientRect();
+    const clickX = clientX - x;
+    const percentageProgressBarClick = (clickX * 100) / width
+    this.multimediaService.seekAudio(percentageProgressBarClick);
   }
 }
