c3c6ffceeaac580fc089a0f3838017fbe3cf33c3
Alejahndro <alejahndro02@gmail.com>
2021-12-02 11:39:54 -0600

    Se comentaron las lineas pertenecientes a las cookies para reliazar las puebas de testeo con jasmine.Spy

--- src/app/modules/auth/services/auth.service.ts
+++ src/app/modules/auth/services/auth.service.ts
@@ -2,17 +2,16 @@ import { Observable } from 'rxjs';
 import { environment } from './../../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { CookieService } from 'ngx-cookie-service';
+// import { CookieService } from 'ngx-cookie-service';//TODO: Para Poder realizar la prueba es necesario comentar la parte de cookie para evita errores en este ejemplo
 import { tap } from 'rxjs/operators';
-
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private readonly URL = environment.api;
   constructor(
-    private http:HttpClient, 
-    private cookie: CookieService) { }
+    private http:HttpClient,
+    /*private cookie: CookieService*/) { }
 
   sendCredentials(email:string, password:string): Observable<any>{
     const body={
@@ -20,11 +19,17 @@ export class AuthService {
       password
     }
     return this.http.post(`${this.URL}/auth/login`, body)
-    .pipe(
-      tap((responseOk:any) =>{
-        const { data,tokenSession } = responseOk;
-        this.cookie.set('token_servicio', tokenSession, 1, '/');//Se guarda la cookie desde el comonete pero tambien se puede desde el servico 
-      })
-    );
+    //TODO Comentar la parte de pipe para realizar la prueba
+    // .pipe(
+    //   tap((responseOk:any) =>{
+    //     const { data,tokenSession } = responseOk;
+    //     this.cookie.set('token_servicio', tokenSession, 1, '/');//Se guarda la cookie desde el comonete pero tambien se puede desde el servico 
+    //   })
+    // );
+  }
+
+  //TODO: ejemplo de testing de una funcion sin metodo post
+  suma(a: number, b:number ):number {
+    return a + b;
   }
 }
