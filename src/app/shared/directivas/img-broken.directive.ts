78c71f843c3374202340317d8302f72b45036e3f
Alejahndro <alejahndro02@gmail.com>
2021-12-04 16:14:52 -0600

    Se declaro por medio de @Input la variable customImg y en el handleError se elaboro la logica para sustituir la imagen de resplado si falla

--- src/app/shared/directivas/img-broken.directive.ts
+++ src/app/shared/directivas/img-broken.directive.ts
@@ -1,13 +1,22 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: 'img[appImgBroken]'
 })
 export class ImgBrokenDirective {
-  @HostListener('error') handleError():void{
+  @Input() customImg:string |boolean = false
+  @HostListener('error')
+  handleError():void{
+    const imgBroken = '../../../assets/images/404.jpg';//Esta imagen se muestra en caso de que llegue a fallar la imagen de error principal
     const elNative = this.elHost.nativeElement
     console.log('esta imagen revento', this.elHost);
-    elNative.src = '../../../assets/images/404.jpg';
+    // elNative.src = '../../../assets/images/404.jpg';
+    if (this.customImg){
+      elNative.src = this.customImg
+    }else{
+      elNative.src = imgBroken
+    }
+
   }
   constructor(private elHost: ElementRef) {
 
@@ -15,3 +24,5 @@ export class ImgBrokenDirective {
    }
 
 }
+
+
